1ad13ef213855285ee5720e7058dd8f93cf4dc60
Sam Lynch <sam.lynch@coolgames.com>
2017-10-26 13:18:26 +0200

    Add line-wrapping to editor

--- js/March22/Globals.js
+++ js/March22/Globals.js
@@ -3,6 +3,7 @@ Globals = function()
 
 	this.aceEditor = ace.edit("textbox");
 	this.aceEditor.$blockScrolling = Infinity;
+	this.aceEditor.getSession().setUseWrapMode(true);
 
 	// This is filled with functions where the parameters are the complete nodes and complete edges, for any modifications.
 	// { func(), storedVariables[] }
